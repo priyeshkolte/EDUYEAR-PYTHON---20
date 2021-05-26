efe2cfc7176671c55cf981563e95fb4bda055541
priyeshkolte <63295838+priyeshkolte@users.noreply.github.com>
2021-05-26 17:31:39 +0530

    Add files via upload

---
+++ Day 3.py
@@ -0,0 +1,41 @@
+#age year calculator
+
+print("welcome to age in years calculator")
+a=input("please enter the year of bitth and press enter  " ) 
+age = int(a)
+result = 2021-age
+print("YOUR AGE IS"+" " + str (result) + " years \n")
+
+
+##simple calculator
+
+
+print(" \n welcome to simple arithmatic claculation operator program")
+
+print("you are requested to enter two number viz. A and B on which the arithmatic operations will be perfored")
+
+a = input("please enter value for A ")
+b = input("please enter value for B ")
+
+A=int(a)
+B=int(b)
+
+add =A+B
+sub =A-B
+div =A/B
+mul =A*B
+mod =A%B
+exp =A**B
+floor_div =A//B
+
+print("A added B = "+ str(add))
+print("A subtracted B = "+ str(sub))
+print("A divided by B = "+ str(div))
+print("A multiplied by B = "+ str(mul))
+print("A modulus of B = "+ str(mod))
+print("A exponent of B = "+ str(exp))
+print("A floor division of B = "+ str(floor_div))
+
+
+print("/n end of Day 3 Assignments")
+
---
+++ Day 4.py
@@ -0,0 +1,40 @@
+print("\n this is the DAY 4 assignment \n")
+
+#letter occurence counter
+print("\n")
+
+print("this is for to count the occurence of letter in a string \n")
+
+string=input("enter string of your choice \n")
+
+letter=input("choose the letter to be count")
+
+print("the count of "+ letter + " in entered string is "+ str(string.count(letter)))
+
+
+
+#string indexing
+
+print("\n")
+
+print("this is for indexing of entered string")
+
+string=input("enter string of you chouce \n")
+
+indexer=input("enter character to index out of entered string")
+
+print(str(string.index(indexer)))
+
+
+
+#string swapper
+
+print("\n")
+
+print("this is for case swaping of entered string")
+
+string=input("enter string of you chouce \n")
+
+scase=string.swapcase()
+
+print(str(scase))
