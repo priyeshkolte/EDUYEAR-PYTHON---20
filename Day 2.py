30293b4a01ace116220e54d9dfa976b8a258de68
priyeshkolte <63295838+priyeshkolte@users.noreply.github.com>
2021-05-26 17:36:05 +0530

    Add files via upload

---
+++ Day 2.py
@@ -0,0 +1,15 @@
+#this day 2 assignment
+
+a = "Priyesh"
+b = "Mumbai"
+
+print ("my name is {}, and I am from city of {}".format(a,b))
+
+print (a)
+print (type (a))
+
+print (b)
+print (type(b))
+
+
+#end of day 2 assignment
